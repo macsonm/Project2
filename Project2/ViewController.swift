320a438f448fcccc0d63aa575f805f1f644872a1
macsonm <75022784+macsonm@users.noreply.github.com>
2022-05-02 19:03:30 +0300

    layout

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -1,19 +1,25 @@
-//
-//  ViewController.swift
-//  Project2
-//
-//  Created by Maxim M on 02.05.2022.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
+    
 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
