84c6c76e8e6e8824c1801feeaac2e6bf3fcb4a52
macsonm <75022784+macsonm@users.noreply.github.com>
2022-05-05 18:41:06 +0300

    add alertController

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -5,21 +5,55 @@ class ViewController: UIViewController {
     @IBOutlet var button2: UIButton!
     @IBOutlet var button3: UIButton!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        
+        var title: String
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += +1
+        } else {
+            title = "Wrong"
+            if score == 0 {
+              score = 0
+            } else {
+                score += -1
+            }
+            
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your core is \(score)", preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        present(ac, animated: true)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    var countries = [String]()
+    var correctAnswer = 0
+    var score = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+       
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor(red: 0.5, green: 0.5, blue: 1, alpha: 0.7).cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        askQuestion()
     }
-    */
-
+    
+    func askQuestion(action: UIAlertAction! = nil) {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        title = countries[correctAnswer].uppercased()
+    }
 }
